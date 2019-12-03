0207d1e5727ad8004bc4cce57e39c1ed156672d3
Xiaoshan He <40234680+xiaoshanhe003@users.noreply.github.com>
2019-12-03 10:52:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,22 +8,27 @@ window.onload = function (){
   var startBtn = document.querySelector('.startBtn');
   var restartBtn = document.querySelector('.restartBtn');
   var scoreEl = document.querySelector('.scoreNum');
+  var panel = document.querySelector('#panel');
 
   startpage.style.display = 'flex';
   restartpage.style.display = 'none';
+  panel.style.display = 'none';
 
   startBtn.addEventListener('click', function (){
     startpage.style.display = 'none';
+    panel.style.display = 'block';
     game.start();
   });
 
   restartBtn.addEventListener('click', function (){
     restartpage.style.display = 'none';
+    panel.style.display = 'block';
     game.restart();
   });
 
   game.failCallback = function (score){
     restartpage.style.display = 'flex';
+    panel.style.display = 'none';
     scoreEl.innerHTML = score;
   };
 };
